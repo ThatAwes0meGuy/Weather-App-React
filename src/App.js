e269385a839a077d897f7d802e1fff438a4f4dac
Ashish <user@xyz.com>
2020-06-13 20:53:16 +0530

    API Working

--- src/App.js
+++ src/App.js
@@ -2,18 +2,59 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Cards from './Components/Cards'
+
+const API_KEY = "78872b499149036415d75305243de5d8";
+
+
 class App extends Component{
 
+  state = {
+    city:"Hyderabad",
+    wdata: {
+      name:'',
+      main: {
+        temp: 0
+      },
+      sys:{
+        country: "IN"
+      }
+    }
+  }
+  
+  getWeather = async () => {
+    // e.preventDefault();
+    const api_Call = await fetch("http://api.openweathermap.org/data/2.5/weather?q="+ this.state.city +"&appid=" + API_KEY + "&units=metric");
+    const data = await api_Call.json();
+    console.log(data);    
+    this.setState({
+      wdata: data
+    })
+  }
+
+
+  changed = (event) => {
+    this.setState({
+      city: event.target.value
+    });
+    
+  }
+
+
   render() {
+
     return(
       <div className="firstPage container-fluid" style={{background:"#0a1f44", paddingTop: "50px"}} >
-      <form>
-          <h1 style={{ color: "white", lineHeight:"1.5", fontSize: "50px" }} >Simple Weather App</h1>
-       <input type="text"/>
-        <button type = "submit" className="btn btn-danger submitButton btn-lg">Submit</button>
-        </form>
-
-        <Cards />
+      {/* <form> */}
+        <h1 style={{ color: "white", lineHeight: "1.5", fontSize: "50px" }} >Simple Weather App</h1>
+          <input type="text" onChange={(event) => this.changed(event)} />
+        <button type = "submit" className="btn btn-danger submitButton btn-lg" onClick={this.getWeather}>Submit</button>
+        {/* </form> */}
+        <Cards
+            City = {this.state.wdata.name}
+            Temp = {this.state.wdata.main.temp}
+            country = {this.state.wdata.sys.country}
+        />
+        
       </div>
     );
   }
--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -2,12 +2,30 @@ import React from "react";
 import Card from "./Card"
 const Cards = (props) =>{
 	return(
-		<div>		
-		<Card />
-			<Card />
-
+		<div style={{ float: "left" }}>
+			<div className="card" style={{ width: "18rem" }}>
+				{/* <img src="..." class="card-img-top" alt="..." /> */}
+				<div className="container">
+					<ul className="cities">
+						<li>
+								<h2> {props.City}
+								<sup>{props.country}</sup>
+							</h2>
+							<div className="city-temp">
+								{props.Temp}
+					<sup>°C</sup>
+							</div>
+							<figure>
+								<img src="" />
+								<figcaption>An elephant at sunset</figcaption>
+							</figure>
+						</li>
+					</ul>
+					{/* <h5 className="card-title"></h5> */}
+					{/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+				</div>
+			</div>
 		</div>
-
 	);
 } 
 export default Cards;
