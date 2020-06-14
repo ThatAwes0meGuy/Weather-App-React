ce3de0e9f1bde3534a43de869578785b3d78f7fa
Ashish <user@xyz.com>
2020-06-14 12:49:37 +0530

    Updates

---
+++ precache-manifest.27b41a3201209575a8d95cc32712af5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61e56e08f6382489e6aff83e501a2c7",
+    "url": "/Weather-App-React/index.html"
+  },
+  {
+    "revision": "e0a636a20eadb153ff22",
+    "url": "/Weather-App-React/static/css/main.d3b3a5c3.chunk.css"
+  },
+  {
+    "revision": "b9277d7794c04d25c7a9",
+    "url": "/Weather-App-React/static/js/2.5b4f1332.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-App-React/static/js/2.5b4f1332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0a636a20eadb153ff22",
+    "url": "/Weather-App-React/static/js/main.719cbb4f.chunk.js"
+  },
+  {
+    "revision": "798f15c4308d9855b84c",
+    "url": "/Weather-App-React/static/js/runtime-main.4eed8c04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Weather-App-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
